07eed683a095dde33c8906f8ed8045e9bdef24fb
rizwanullah313 <ru323091@gmail.com>
2021-10-06 15:27:10 +0500

    successfully connection created with mongo database

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 // async higa qk mongosse async kaam kerta h
 module.exports =  connect = async () => {
     try{
-    const response = await mongoose.connect(process.env.URL);
+    const response = await mongoose.connect(process.env.URL, {useUnifiedTopology: true, useNewUrlParser: true});
     console.log("Connection created");
     }
     catch(error){
