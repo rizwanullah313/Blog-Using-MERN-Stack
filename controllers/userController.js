63a25fbf02da8d4c09043003eaebf021d675ac78
rizwanullah313 <ru323091@gmail.com>
2021-10-07 17:06:28 +0500

    login successfully with token

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,11 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require('dotenv').config()
 
-
+const createToken = (user)=>{
+    return jwt.sign({user}, process.env.SECRET,{
+        expiresIn: '7d'
+    });
+}
 
 // code for validation
 // npm install --save express-validator
@@ -42,9 +46,10 @@ module.exports.register = async (req, res) => {
 				email,
 				password: hash,
 			});
-			const token = jwt.sign({user}, process.env.SECRET,{
-                expiresIn: '7d'
-            });
+            const token = createToken(user);
+			// const token = jwt.sign({user}, process.env.SECRET,{
+            //     expiresIn: '7d'
+            // });
 			return res
 				.status(200)
 				.json({ msg: 'Your account has been created', token });
@@ -65,10 +70,58 @@ module.exports.loginValiations = [
 	body('email').not().isEmpty().trim().withMessage('Email is required'),
 	body('password').not().isEmpty().withMessage('Password is required'),
 ];
+module.exports.login = async (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+	const { email, password } = req.body;
+	try {
+		const user = await User.findOne({ email });
+		if (user) {
+			const matched = await bcrypt.compare(password, user.password);
+			if (matched) {
+				const token = createToken(user);
+				return res
+					.status(200)
+					.json({ msg: 'You have login successfully', token });
+			} else {
+				return res
+					.status(401)
+					.json({ errors: [{ msg: 'Password is not correct' }] });
+			}
+		} else {
+			return res.status(404).json({ errors: [{ msg: 'Email not found' }] });
+		}
+	} catch (error) {
+		return res.status(500).json({ errors: error });
+	}
+};
 
-module.exports.login = (req, res)=>{
-    const errors =  validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()});
-    }
-}
+// module.exports.login = async (req, res)=>{
+//     const errors =  validationResult(req);
+//     if(!errors.isEmpty()){
+//         return res.status(400).json({errors: errors.array()});
+//     }
+//     const {email, password} = req.body;
+//    try{
+//        const user = await User.findOne({email});
+
+//     if(user){
+//         const matched =  await bcrypt.compare(password, user.password);
+//    if(matched){
+//       const user = createtoken(user);
+//       return res.status(200).json({msg: " You have login successfully", token})
+//    }
+//     else{
+//         return res.status(401).json({errors: [{msg: 'Password is not correct'}]})
+//     }
+// }
+//     else{
+//       return res.status(404).json({errors: [{msg: 'Email Not Found'}]})
+//     }}
+//     catch(error)
+//     {
+//         return res.status(500).json({errors: error})
+//     }
+// };
